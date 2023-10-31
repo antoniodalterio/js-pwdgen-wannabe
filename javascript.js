99d0d40feab6a63a77851e629132f932004b9801
antoniodalterio <antoniodalterio2227@gmail.com>
2023-10-31 17:46:52 +0100

    feat: var numero casuale

--- javascript.js
+++ javascript.js
@@ -5,3 +5,13 @@ console.log(personNome);
 
 const personCognome = prompt('scrivi il tuo cognome');
 console.log(personCognome);
+
+const personColor = prompt('scrivi il tuo colore preferito');
+console.log(personColor);
+const numCasuale = Math.floor(Math.random(1) * 100);
+console.log(numCasuale);
+
+const passGenerator = personNome + personCognome + personColor + numCasuale;
+console.log(passGenerator);
+
+document.getElementById('pass').innerHTML = 'Password:' + ' ' + passGenerator;
