4f72bccdf5d2a62029d4705a221696a2a008577d
antoniodalterio <antoniodalterio2227@gmail.com>
2023-10-31 17:16:24 +0100

    feat: prime due variabili e doc README

---
+++ javascript.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const personNome = prompt('scrvi il tuo nome');
+console.log(personNome);
+
+const personCognome = prompt('scrivi il tuo cognome');
+console.log(personCognome);
---
+++ readme.md
@@ -0,0 +1,4 @@
+#Password insicura
+
+fornire all'utente un generatore di password in base a delle domande,
+il generatore è stato crato con javascript
